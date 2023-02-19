69e1bcfd41a901aa55db7443bd83f70d587ca41c
Eduardo.oliveira <eduardo.oliveira@elastic.fi>
2023-02-19 18:13:39 -0300

    importando shakra ui

---
+++ front/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import ListAcoes from "./ListAcoes";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="content">
+          <ListAcoes />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
